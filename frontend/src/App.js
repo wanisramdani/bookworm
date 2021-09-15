2b17452d6a7b90ff19699ca29b18017235487cef
Repu <wanisramdani00@gmail.com>
2021-09-15 10:35:03 +0100

    Setup react project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import { Container, Box } from '@material-ui/core';
+import { Navbar, Main} from './components'
+
+import useStyles from './Styles';
+/* https://github.com/adrianhajdin/project_e_commerce/blob/main/src/App.js */
+
+const Section = lazy(() => import('./components/Section/Section'))
+
+const theme = createTheme({
+  direction: 'rtl',
+  typography: {
+    fontFamily: 'Cairo',
+  },
+})
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Container>
+              <Box className={classes.content}>
+
+                  <Box className={classes.header}>
+                    <Box className={classes.nav}>
+                      <Navbar />
+                    </Box>
+                  </Box>
+
+                  <Box className={classes.main}>
+                    <Main />
+                  </Box>
+                  
+                  <Box className={classes.section}>
+                    <Suspense fallback={<div>Loading...</div>}>
+                      <Section title="البطاقات الدعوية" />
+                      <Section title="كتب السنة" />
+                      <Section title="قسم المرئيات" />  
+                    </Suspense>
+                  </Box>
+
+                  <Box className={classes.footer}>
+
+                  </Box>
+              </Box>
+          </Container>
+          </Route>
+
+
+        </Switch>
+
+      </Router>
+    </ThemeProvider>
   );
 }
 
