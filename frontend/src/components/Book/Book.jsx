808341d2f157780f0bf0a76e352c18ac8c0085d3
Repu <wanisramdani00@gmail.com>
2021-09-16 08:43:03 +0100

    Add book

---
+++ frontend/src/components/Book/Book.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import useStyles from './Styles';
+
+const Book = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Book
